932e9fe6a62ca26df63bcd1f6249e0ff5ebe313c
nemo <yimoxia@Outlook.com>
2022-05-03 20:10:01 +0800

    Update README

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyFDown", 
-    version="0.0.2",
+    version="0.0.3",
     description="A fast downloader with Python",
     license="MIT",
-    long_description="""
-        This library is a fast downloader that like Aria2. But it doesn't need to compile. It is easy to use. 
-    """,
+    long_description=open("README.md").read(),
     author="Nemo2011",
     author_email="yimoxia@outlook.com",
     packages=find_packages("PyFDown", exclude=["test.py"]),
     install_requires=['requests', 'tqdm', 'multitasking', 'retry'],
-    url = "https://github.com/Nemo2011/PyFDown"
+    url = "https://github.com/Nemo2011/PyFDown", 
 )
