55ba85f6b422805fb92860e5073b4dc1939c8f19
nemo <yimoxia@Outlook.com>
2022-05-03 20:00:07 +0800

    Fix the bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyFDown", 
-    version="0.0.1",
+    version="0.0.2",
     description="A fast downloader with Python",
     license="MIT",
     long_description="""
